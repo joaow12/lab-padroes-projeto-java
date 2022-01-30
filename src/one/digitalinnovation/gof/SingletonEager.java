b4e39f2012009e224ed28d5949dfe8619bd46d3f
João Victor <jujotasurf@gmail.com>
2022-01-30 17:23:22 -0300

    primeiro commit

---
+++ src/one/digitalinnovation/gof/SingletonEager.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof;
+
+public class SingletonEager {
+	
+	private static SingletonEager instancia;
+	
+	private SingletonEager(){
+		super();
+	}
+	
+	public static SingletonEager getInstancia() {
+		if(instancia == null) {
+			instancia = new SingletonEager();
+		}
+		return instancia;
+	}
+	
+	
+
+}
